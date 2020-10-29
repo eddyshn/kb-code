29baf118a5d25fe3fea0e3f2011f661bd04d4580
Eddy Shan <eddy.shan@outlook.com>
2020-10-29 08:22:40 +0800

    update

---
+++ 00-javascript/module/commonjs-node/2.js
@@ -0,0 +1,19 @@
+const phone = {
+    brand: 'apple',
+    model: 'x'
+}
+
+
+const book = {
+    author: 'eddy',
+    price: 100
+}
+
+
+module.exports.person = {
+    name: 'eddy'
+}
+
+module.exports.phone = phone;
+
+module.exports.book = book;
---
+++ 00-javascript/module/commonjs-node/test.js
@@ -0,0 +1,14 @@
+const car = require('./1')
+
+console.log(car);
+
+
+// const phone = require('./2').phone;
+// const book = require('./2').book;
+// const person = require('./2').person;
+
+const {phone, book, person} =  require('./2');
+
+console.log(phone);
+console.log(book);
+console.log(person);
